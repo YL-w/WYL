1d397bb46dc743395643daa19e54d516baa1ce77
wangyongliang <Asus@192.168.64.1>
2019-06-12 16:19:16 +0800

    222

--- Test/src/wyk.java
+++ Test/src/wyk.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
 
 public class wyk {
+	public static void main(String[] args) {
+		Scanner a = new Scanner(System.in);
+		System.out.print("输入数字");
+		int b = a.nextInt();
+		if (b < 100 && b > 0) {
+			System.out.println("你还是个人");
 
+		} else {
+			System.out.println("你是什么怪物");
+		}
+
+	}
 }
